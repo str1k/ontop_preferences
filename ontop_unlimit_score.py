306981b924e0c98a464403cdf09c4f3c35fa36b6
str1k <y.thanasit@gmail.com>
2018-06-14 10:37:00 +0700

    add column to calculation and add logging to unlimit score script

--- ontop_unlimit_score.py
+++ ontop_unlimit_score.py
@@ -26,21 +26,53 @@ if __name__ == "__main__":
     conf = SparkConf().setAppName("ontop_unlimit_score")
     sc = SparkContext(conf=conf)
     sqlContext = SQLContext(sc)
-    ontop_preferences = sqlContext.read.format("com.databricks.spark.csv").option("header", "true").option("inferSchema", "true").option("delimiter", '|').load('/preprocessed_cvm/package_preferences_rowId_3')
-    customer_persona = sqlContext.read.format("com.databricks.spark.csv").option("header", "true").option("inferSchema", "true").option("delimiter", '|').load('/preprocessed_cvm/customer_persona_rowId_3')
+    print("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n###################################################################################################")
+    print("Start Calculating Ontop unlimit Score")
+    print("###################################################################################################")
+
+    print("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n###################################################################################################")
+    print("Start Reading Ontop Preferences Data")
+    print("###################################################################################################")
+    ontop_preferences = sqlContext.read.format("com.databricks.spark.csv").option("header", "true").option("inferSchema", "true").option("delimiter", '|').load('/preprocessed_cvm/package_preferences_rowId')
+    print("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n###################################################################################################")
+    print("Finished Reading Ontop Preferences Data")
+    print("###################################################################################################")
+
+    print("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n###################################################################################################")
+    print("Start Reading Customer Preferences Data")
+    print("###################################################################################################")
+    customer_persona = sqlContext.read.format("com.databricks.spark.csv").option("header", "true").option("inferSchema", "true").option("delimiter", '|').load('/preprocessed_cvm/customer_persona_rowId')
+    print("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n###################################################################################################")
+    print("Finished Reading Customer Preferences Data")
+    print("###################################################################################################")
+
+    print("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n###################################################################################################")
+    print("Start Creating Customer Preferences Block Matrix")
+    print("###################################################################################################")
     index_ct = customer_persona.drop("analytic_id")
     index_anaId = customer_persona.select("id","analytic_id")
     index_ct.registerTempTable("index_ct")
-    ontop_pref_price = ontop_preferences.select("id","Package_Size_64Kbps","Package_Size_384Kbps","Package_Size_512Kbps","Package_Size_1Mbps","Package_Size_2Mbps","Package_Size_4Mbps","Package_Size_6Mbps")
+    ontop_pref_price = ontop_preferences.select("id","Package_Size_64Kbps","Package_Size_384Kbps","Package_Size_512Kbps","Package_Size_1Mbps","Package_Size_2Mbps","Package_Size_4Mbps","Package_Size_6Mbps","Package_Size_Unlimit_Timed","Package_Size_Unlimit")
     ontop_pref_price = ontop_pref_price.orderBy(asc("id"))
     bmB_1 = IndexedRowMatrix(ontop_pref_price.rdd.map(lambda x: IndexedRow(x[0], Vectors.dense(x[1:])))).toBlockMatrix(rowsPerBlock=222)
     count = customer_persona.count()
+    print("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n###################################################################################################")
+    print("Finished Creating Customer Preferences Block Matrix")
+    print("###################################################################################################")
+
     loop = int(count/200000)
     startId = 1
     i = 0
     res = index_ct
     del customer_persona
+    print("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n###################################################################################################")
+    print("Start Matrix Multiplication")
+    print("Number of loop: " + str(loop))
+    print("###################################################################################################")
     for x in range(loop):
+        print("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n###################################################################################################")
+        print("Starting partial Matrix Multiplication: "+str(x+1) + "/" +str(loop))
+        print("###################################################################################################")
         if x != loop-1:
             batch = sqlContext.sql("SELECT * FROM index_ct WHERE id BETWEEN " + str(i+1) + " AND " + str(i+200001))
             matA = IndexedRowMatrix(batch.rdd.map(lambda row: IndexedRow(row[0], Vectors.dense(row[1:]))))
@@ -49,7 +81,7 @@ if __name__ == "__main__":
             tmp = bm_Package_Size_384Kbpscore_result.toIndexedRowMatrix().rows.sortBy(lambda x : x.index).map(lambda x:  (x.index,x.vector)).toDF(["id", "vector"])
             tmp = tmp.rdd.map(extract).toDF(["id"])
             tmp = tmp.join(index_anaId, ["id"], "left_outer")
-            tmp = tmp.selectExpr("analytic_id", "id", "_2 as Package_Size_64Kbps", "_3 as Package_Size_384Kbps","_4 as Package_Size_512Kbps", "_5 as Package_Size_1Mbps","_6 as Package_Size_2Mbps","_7 as Package_Size_4Mbps","_8 as Package_Size_6Mbps")
+            tmp = tmp.selectExpr("analytic_id", "id", "_2 as Package_Size_64Kbps", "_3 as Package_Size_384Kbps","_4 as Package_Size_512Kbps", "_5 as Package_Size_1Mbps","_6 as Package_Size_2Mbps","_7 as Package_Size_4Mbps","_8 as Package_Size_6Mbps","_9 as Package_Size_Unlimit_Timed","_10 as Package_Size_Unlimit")
             if i == 0:
                 #tmp.repartition(1).write.option("sep","|").option("header","true").csv("/ontop_pref/tmp_score_" + str(i))
                 res = tmp
@@ -64,8 +96,18 @@ if __name__ == "__main__":
             tmp = bm_Package_Size_384Kbpscore_result.toIndexedRowMatrix().rows.sortBy(lambda x : x.index).map(lambda x:  (x.index,x.vector)).toDF(["id", "vector"])
             tmp = tmp.rdd.map(extract).toDF(["id"])
             tmp = tmp.join(index_anaId, ["id"], "left_outer")
-            tmp = tmp.selectExpr("analytic_id", "id", "_2 as Package_Size_64Kbps", "_3 as Package_Size_384Kbps","_4 as Package_Size_512Kbps", "_5 as Package_Size_1Mbps","_6 as Package_Size_2Mbps","_7 as Package_Size_4Mbps","_8 as Package_Size_6Mbps")
+            tmp = tmp.selectExpr("analytic_id", "id", "_2 as Package_Size_64Kbps", "_3 as Package_Size_384Kbps","_4 as Package_Size_512Kbps", "_5 as Package_Size_1Mbps","_6 as Package_Size_2Mbps","_7 as Package_Size_4Mbps","_8 as Package_Size_6Mbps","_9 as Package_Size_Unlimit_Timed","_10 as Package_Size_Unlimit")
             res = res.unionAll(tmp)
-            res.write.option("sep","|").option("header","true").csv("/ontop_pref/ontop_duration_score")
+            print("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n###################################################################################################")
+            print("Finished Matrix Multiplication")
+            print("###################################################################################################")
+
+            print("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n###################################################################################################")
+            print("Start Writting Output file")
+            print("###################################################################################################")
+            res.write.option("sep","|").option("header","true").csv("/ontop_pref/ontop_unlimit_score")
+            print("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n###################################################################################################")
+            print("Finished Writting Output file")
+            print("###################################################################################################")
         i = i+1
     sc.stop()
